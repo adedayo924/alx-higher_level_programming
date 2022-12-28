93bd6394d5432ac1ae8fd7666c7c09e7ff13aa76
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2022-12-28 13:16:29 +0100

    Solutions to python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+"""contains the function find_peak"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
-
-    if list_of_integers is None or list_of_integers == []:
+    """finds a peak in a list of unsorted integers"""
+    if not list_of_integers:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    length = len(list_of_integers)
+    half = int(length / 2)
+
+    # Validate Length only 3.
+    if half + 1 >= length and half - 1 < 0:
+        return list_of_integers[half]
+    elif half - 1 < 0:
+        if list_of_integers[half] > list_of_integers[half + 1]:
+            return list_of_integers[half]
+        else:
+            return list_of_integers[half + 1]
+    elif half + 1 >= length:
+        if list_of_integers[half] > list_of_integers[half - 1]:
+            return list_of_integers[half]
+        else:
+            return list_of_integers[half - 1]
+
+    if list_of_integers[half - 1] < list_of_integers[half]\
+       > list_of_integers[half + 1]:
+        return list_of_integers[half]
+
+    if list_of_integers[half + 1] > list_of_integers[half - 1]:
+        return find_peak(list_of_integers[half:])
+
+    return find_peak(list_of_integers[:half])
